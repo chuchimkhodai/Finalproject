d67cbfa78f87dd5003980fa8b16cef81adb33d86
Long Vu <longvt.it4@vnpt.vn>
2025-10-03 09:02:37 +0700

    commit

--- firebaseconfig.js
+++ firebaseconfig.js
@@ -1,21 +1,25 @@
 // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-analytics.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
+import { getFirestore, collection, addDoc, getDocs, query, orderBy, serverTimestamp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyBVK76xOZ-cO3I4jSA1r0u2WSJ5p43EYXg",
-    authDomain: "fc26-minhphuc.firebaseapp.com",
-    projectId: "fc26-minhphuc",
-    storageBucket: "fc26-minhphuc.firebasestorage.app",
-    messagingSenderId: "705880668674",
-    appId: "1:705880668674:web:5297a313fdabbec564c51b",
-    measurementId: "G-R9BMG3FWJ1"
-  };
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBVK76xOZ-cO3I4jSA1r0u2WSJ5p43EYXg",
+  authDomain: "fc26-minhphuc.firebaseapp.com",
+  projectId: "fc26-minhphuc",
+  storageBucket: "fc26-minhphuc.firebasestorage.app",
+  messagingSenderId: "705880668674",
+  appId: "1:705880668674:web:5297a313fdabbec564c51b",
+  measurementId: "G-R9BMG3FWJ1"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+// Export để sử dụng trong các file khác
+export { auth, db, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged, collection, addDoc, getDocs, query, orderBy, serverTimestamp };
